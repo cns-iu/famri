288ad989868708084bbd4fdb00a35c7efec23c1e
Bruce Herr II <bherr@indiana.edu>
2018-04-17 09:40:32 -0400

    Extract year for grants (ugly, WIP)

--- packages/famri-database/src/loader/options.ts
+++ packages/famri-database/src/loader/options.ts
@@ -1,4 +1,5 @@
 export const GRANTS = '../../raw-data/FAMRI_zipcode_info_MG.xlsx';
+export const GRANTS_W_YR = '../../raw-data/FAMRI_2002-2018 Studies with year .xlsx';
 export const PUBS = '../../raw-data/FAMRI_Publications by grant_final .xlsx';
 export const CLEAN_PUBS = '../../raw-data/My EndNote Library_FAMRI_REFERENCE_GROUP_final.json';
 export const DB_JSON = '../../raw-data/database.json';
