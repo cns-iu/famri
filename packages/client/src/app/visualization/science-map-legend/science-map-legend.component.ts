d23f86810e08f362336fabf93dd0b5328454485e
Bruce Herr II <bherr@indiana.edu>
2018-04-19 14:42:35 -0400

    Added # un science mapped pubs on legend

--- packages/client/src/app/visualization/science-map-legend/science-map-legend.component.ts
+++ packages/client/src/app/visualization/science-map-legend/science-map-legend.component.ts
@@ -7,9 +7,9 @@ import {
   Output,
   SimpleChanges
 } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import { BoundField } from '@ngx-dino/core';
-
 import { Filter, SubdisciplineWeight } from 'famri-database';
 
 import { subdisciplineSizeField } from '../shared/science-map/science-map-fields';
@@ -18,35 +18,32 @@ import { ScienceMapDatabaseService } from '../shared/science-map/science-map-dat
 @Component({
   selector: 'famri-science-map-legend',
   templateUrl: './science-map-legend.component.html',
-  styleUrls: ['./science-map-legend.component.sass'],
-  providers: [ScienceMapDatabaseService]
+  styleUrls: ['./science-map-legend.component.sass']
 })
 export class ScienceMapLegendComponent implements OnInit, OnChanges {
   @Input() filter: Partial<Filter> = {};
   @Output() filterUpdateComplete = new EventEmitter<boolean>();
 
   subdisciplineSize: BoundField<string>;
-  filteredSubdisciplines: SubdisciplineWeight[];
+  filteredSubdisciplines: Observable<SubdisciplineWeight[]>;
+  unmappedSubdisciplines: Observable<SubdisciplineWeight>;
 
   nodeSizeEncoding = '# Fractionally Assigned Papers';
 
-  constructor(private dataService: ScienceMapDatabaseService) { }
+  constructor(private dataService: ScienceMapDatabaseService) {
+    this.filteredSubdisciplines = dataService.filteredSubdisciplines.asObservable();
+    this.unmappedSubdisciplines = dataService.unmappedSubdisciplines.asObservable();
+  }
 
   ngOnInit() {
-    this.filteredSubdisciplines = [];
-
-    // not user facing
     this.subdisciplineSize = subdisciplineSizeField.getBoundField('size');
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (('filter' in changes) && this.filter) {
-        this.dataService.fetchData(this.filter).subscribe(
-          undefined, undefined, () => this.filterUpdateComplete.emit(true)
-        );
-      }
-    this.dataService.filteredSubdisciplines.subscribe((subdisciplines) => {
-      this.filteredSubdisciplines = subdisciplines;
-    });
+      this.dataService.fetchData(this.filter).subscribe(undefined, undefined,
+        () => this.filterUpdateComplete.emit(true)
+      );
+    }
   }
 }
